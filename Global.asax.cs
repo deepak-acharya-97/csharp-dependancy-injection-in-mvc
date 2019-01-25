6fe95d73a8be8b133f67d7e24a7da9db8e2b7c0c
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-25 15:05:57 +0530

    Added DI in Global.asax.cs file

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using DIMVCPracticeUpdated.Interfaces;
+using DIMVCPracticeUpdated.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Unity;
 
 namespace DIMVCPracticeUpdated
 {
@@ -16,6 +19,12 @@ namespace DIMVCPracticeUpdated
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<IUserSession, UserSession>();
+
+            DependencyResolver.SetResolver(new PracticeDependencyResolver(container));
         }
     }
 }
